a0e0912e17bfee1da1b9f1818b8bad27e8677877
William <daniavilab24@gmail.com>
2020-01-22 21:21:30 +0100

    ReparticionTrabajos
    Tareas pendientes

--- server/main.js
+++ server/main.js
@@ -1,8 +1,11 @@
-function Jugador() {
+var tamanoTablero=20;
+var tablero[tamanoTablero][tamanoTablero];
+
+function Jugador(nombre) {
+    this.nombre=nombre;
     this.puntuacion=0;
     this.miTanque=new Tanque();
 };
-
 
 function Tanque() {
 
@@ -18,17 +21,31 @@ function Tanque() {
     this.retraso=3;
     this.vidas=2;
     this.horaUltimoDisparo;
+    //JAIRO: Asignacion de imagen.
     this.imagen;
     //Asignamos una posicion del canon por defecto.
     this.posicionCanon=0;
     //Metodos
-    this.dispara=function() {}
+    /*
+    TODO:
+    this.dispara=function(){
+        //Tener en cuenta la posicion canon
+        if(horaUltimoDisparo-horaActual>=retraso){
+            //Mata.
+        }
+    }
+    */
+    //JULIO: Genera posicion: Devolver un valor al azar en el tablero.
     this.generaPosicion=function(){}
+    //MANOLO: Modifica las variables posicionX y posicionY del tanque en funcion metodo.
+    //Mueve de uno en uno.
+    //TODO: Mantener pulsado.
     this.mueveDerecha=function(){}
     this.mueveIzquierda=function(){}
     this.mueveArriba=function(){}
     this.mueveAbajo=function(){}
-    this.mueve=function (direccion) {
+    //llama a un metodo u otro en funcion del parametro pasado.
+    this.mueve=function (direccion){
         switch(direccion)
         {
             case 0:
@@ -47,6 +64,6 @@ function Tanque() {
                 break;
         }
     }
-
+    //TODO: Getters & Setters
 };
 

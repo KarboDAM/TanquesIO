def33ecb231b44c2642c1d8348207b78139169cd
Julio <Du_riest@hotmail.com>
2020-01-24 17:44:18 +0100

    Update main.js
    Vidas, posicion de canon y generar posicion

--- server/main.js
+++ server/main.js
@@ -22,8 +22,53 @@ function Tanque() {
     //Asignamos una posicion del canon por defecto.
     this.posicionCanon=0;
     //Metodos
+<<<<<<< Updated upstream
     this.dispara=function() {}
     this.generaPosicion=function(){}
+=======
+    /*
+    TODO:
+    this.dispara=function(){
+        //Tener en cuenta la posicion canon
+        if(horaUltimoDisparo-horaActual>=retraso){
+            //Mata.
+        }
+    }
+    */
+    //JULIO: Genera posicion: Devolver un valor al azar en el tablero.
+	
+	
+	
+	function restarVida(){
+		
+		this.vidas=this.vidas-1;
+		
+		
+	}
+	function getVidas(vidas){
+		
+		return this.vidas;
+
+	}
+	
+	
+	function getPosicionCanon(){
+		
+		return this.posicionCanon;
+		
+		
+	}
+    function generaPosicion(){
+		
+		return Math.floor(Math.random()*tamanoTablero);
+		//this.pos_y=Math.floor(Math.random()*tamanoTablero);
+		}
+		
+	}
+    //MANOLO: Modifica las variables posicionX y posicionY del tanque en funcion metodo.
+    //Mueve de uno en uno.
+    //TODO: Mantener pulsado.
+>>>>>>> Stashed changes
     this.mueveDerecha=function(){}
     this.mueveIzquierda=function(){}
     this.mueveArriba=function(){}
